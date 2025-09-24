5b04d406f0626c897a81501385ab3327d646c4fc
Anurag Singh <as040999@gmail.com>
2025-09-24 19:10:10 +0100

    modofied the structure and location on local

--- ice_cream_sales_prediction.py
+++ ice_cream_sales_prediction.py
@@ -9,14 +9,14 @@ from pymongo.server_api import ServerApi
 # Connect to the MongoDB cluster to store the inputs and the prediction
 uri = "mongodb+srv://anurag:07121998@cluster0.ugo9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
-db = client['ice_cream_sales']  # Create a new database
-collection = db['ice_cream_sales_pred'] # Create a new collection/table in the database
+db = client['Ice_Cream_Sales']     # Create a new database
+collection = db['Ice_Cream_Sales_Prediction']     # Create a new collection/table in the database
 
 def load_model():
-    with open('ice_cream_sales_final_model.pkl', 'rb') as f:
-        poly_model2 = pickle.load(f)
+    with open('ice_cream_sales_polynomial_regression_final_model.pkl', 'rb') as f:
+        poly_model3 = pickle.load(f)
 
-    return poly_model2
+    return poly_model3
 
 def preprocesssing_input_data(data):
     df = pd.DataFrame([data])
@@ -32,7 +32,7 @@ def predict_data(data):
 
 def main():
     st.title("Ice Cream Sales Prediction")
-    st.write("Enter the temperature to get a prediction for your Ice Cream Sales (units)")
+    st.write("Enter the temperature below to get a prediction for your Ice Cream Sales (units)")
 
     temprature = st.number_input("Temperature (°C)", value = 20.0) 
 
@@ -44,9 +44,9 @@ def main():
         prediction = predict_data(user_data)
         st.success(f"Your prediction result is: {round(float(prediction[0]), 3)}")
 
-        user_data["prediction"] = round(float(prediction[0]), 3)    # Add the prediction to the user_data dictionary
+        user_data["prediction"] = round(float(prediction[0]), 3)     # Add the prediction to the user_data dictionary
         user_data = {key: int(value) if isinstance(value, np.integer) else float(value) if isinstance(value, float) else value for key, value in user_data.items()}    # Convert the values to int or float if they are of type np.integer or np.float
-        collection.insert_one(user_data)    # Insert the user_data dictionary/record to the MongoDB collection
+        collection.insert_one(user_data)     # Insert the user_data dictionary/record to the MongoDB collection
 
 if __name__ == "__main__":
     main()
